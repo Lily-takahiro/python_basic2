d4bd700dffb2be72da6143e92a9a08aabd9243a7
Lily-takahiro <tttsss120604280520@gmail.com>
2025-08-16 13:06:52 +0900

    課題B修正終了

--- b-1.py
+++ b-1.py
@@ -1,6 +1,5 @@
 for i in range(1, 10):
     for j in range(1, 10):
 
-        print((i * j), end="  ")
+        print((i * j), end=" ")
     print()  # 各行の最後で改行
-
--- beautiful_kuku_b-3.py
+++ beautiful_kuku_b-3.py
@@ -1,8 +1,10 @@
+
 rows = int(input("行数を入力してください:"))
 cols = int(input("列数を入力してください:"))
 
 
-for i in range(1, rows + 1):
-    for j in range(1, cols + 1):
-        print(f"{i}X{j}={i * j:2}", end=" ")
-print()  # 改行
+for j in range(1, rows + 1):  # 行（左側の数）
+    for i in range(1, cols + 1):  # 列（右側の数）
+        print(f"{i} x {j} = {i*j:2}", end=" | ")
+    print()  # 改行
+
